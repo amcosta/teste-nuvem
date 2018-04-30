e08c88023c1ec058a16cd21b51865199fc6d4025
Alex Moreno <alex.moreno.costa@gmail.com>
2018-04-30 08:56:38 -0300

    Inserido comentários no AddressServiceFactory

--- src/TiendaNube/Checkout/Service/Shipping/AddressServiceFactory.php
+++ src/TiendaNube/Checkout/Service/Shipping/AddressServiceFactory.php
@@ -5,6 +5,10 @@ namespace TiendaNube\Checkout\Service\Shipping;
 use Psr\Log\LoggerInterface;
 use TiendaNube\Checkout\Service\Store\StoreServiceInterface;
 
+/**
+ * Class AddressServiceFactory
+ * @package TiendaNube\Checkout\Service\Shipping
+ */
 class AddressServiceFactory
 {
     /**
@@ -22,6 +26,12 @@ class AddressServiceFactory
      */
     private $storeService;
 
+    /**
+     * AddressServiceFactory constructor.
+     * @param \PDO $pdo
+     * @param LoggerInterface $logger
+     * @param StoreServiceInterface $storeService
+     */
     public function __construct(\PDO $pdo, LoggerInterface $logger, StoreServiceInterface $storeService)
     {
         $this->pdo = $pdo;
@@ -29,6 +39,12 @@ class AddressServiceFactory
         $this->storeService = $storeService;
     }
 
+    /**
+     * Return a AddressServiceBeta if the store is a beta tester otherwise return a AddressService
+     *
+     * @return AddressServiceInterface
+     * @throws \RuntimeException
+     */
     public function create()
     {
         $store = $this->storeService->getCurrentStore();
